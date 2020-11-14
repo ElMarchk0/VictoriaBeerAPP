6ae68f32d033841498d0238e7e37396f73f181e8
ElMarchk0 <67571255+ElMarchk0@users.noreply.github.com>
2020-11-13 22:57:28 -0800

    First draft satisfactory, docker next

--- routes/beers.js
+++ routes/beers.js
@@ -2,6 +2,29 @@ const beerRoutes = (app, fs) => {
     // variables
     const dataPath = './BeerData/beersV1.json';
 
+    // helper methods
+    const readFile = (callback, returnJson = false, filePath = dataPath, encoding = 'utf8') => {
+        fs.readFile(filePath, encoding, (err, data) => {
+            if (err) {
+                throw err;
+            }
+
+            callback(returnJson ? JSON.parse(data) : data);
+        });
+    };
+
+    const writeFile = (fileData, callback, filePath = dataPath, encoding = 'utf8') => {
+
+        fs.writeFile(filePath, fileData, encoding, (err) => {
+            if (err) {
+                throw err;
+            }
+
+            callback();
+        });
+    };
+
+
     // READ
     app.get('/beers', (req, res) => {
         fs.readFile(dataPath, 'utf8', (err, data) => {
@@ -13,5 +36,8 @@ const beerRoutes = (app, fs) => {
         });
     });
 };
+
+
+
 
 module.exports = beerRoutes;
